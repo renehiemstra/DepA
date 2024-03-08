862944995acf459678fb53fcb14d28e267b2bca3
rrhiemstar <rrhiemstar@gmail.com>
2024-03-08 11:36:00 +0100

    <release> DepA..v0.1.1

--- Project.lua
+++ Project.lua
@@ -2,7 +2,7 @@ Project = {
   name = "DepA",
   uuid = "293c2656-20e9-4376-9642-e8df3a1d3d60",
   authors = {"rrhiemstar<rrhiemstar@gmail.com>", }, 
-  version = "0.1.0",
+  version = "0.1.1",
   deps = {
         DepDep = "0.2.0",
     },
